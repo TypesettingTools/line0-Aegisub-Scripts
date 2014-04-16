38a3c19ca33a529b016774174b1f8836c760bf10
line0 <line0@mail.lv>
2014-04-16 21:41:24 +0200

    add Remove Unpainted Paths.js

---
+++ Remove Unpainted Paths.js
@@ -0,0 +1,21 @@
+﻿var sel = app.activeDocument.selection;
+for (var i=0; i<sel.length; i++) {
+	if(sel[i].typename == "CompoundPathItem") {
+		pathItems = sel[i].pathItems;
+		for(var j=0; j<pathItems.length; j++)  {
+			alert(pathItems.length);
+			if (removeUnpaintedPath(pathItems[j])) {j--}
+		}
+	} else { removeUnpaintedPath(sel[i]); }
+}
+
+
+function removeUnpaintedPath(pathItem)
+{
+		if(pathItem.typename == "PathItem" && !(pathItem.filled || pathItem.stroked))
+		{
+			pathItem.remove();
+			return true;
+		}
+		else return false;
+}

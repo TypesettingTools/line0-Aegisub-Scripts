a8c8ccbb58447123a7f9d63f9d08a4835a1a3976
line0 <line0@mail.lv>
2014-04-17 17:06:24 +0200

    add Remove Unpainted Paths (Active Document)

---
+++ Remove Unpainted Paths (Active Document).js
@@ -0,0 +1,44 @@
+﻿var sel = app.activeDocument.pageItems;
+for (var i=0; i<sel.length; i++) {
+	if (processPageItem(sel[i])) {i--}
+}
+
+
+function removeUnpaintedPath(pathItem)
+{
+		if(pathItem.typename == "PathItem" && !(pathItem.filled || pathItem.stroked))
+		{
+			pathItem.remove();
+			return true;
+		}
+		else return false;
+}
+
+function recurseGroup(groupItem)
+{
+	for(var i=0; i<groupItem.pageItems.length; i++) {
+		if(processPageItem(groupItem.pageItems[i])) {i--}
+	}
+	if (groupItem) return false else return true; 
+}
+
+
+function recurseCompound(compoundPathItem)
+{
+	for(var i=0; i<compoundPathItem.pathItems.length; i++) {
+		if(processPageItem(compoundPathItem.pathItems[i])) {i--}
+	}
+	if (compoundPathItem) return false else return true; 
+}
+
+function processPageItem(pageItem)
+{
+	if(pageItem.typename == "CompoundPathItem") {
+		return recurseCompound(pageItem);
+	}
+	else if(pageItem.typename == "GroupItem") {
+		return recurseGroup(pageItem);
+	} else {
+		 return removeUnpaintedPath(pageItem); 
+	}
+}

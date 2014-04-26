04dc8f2f4fd261ae65aa1fc2cf7a5dd616097e4d
line0 <line0@mail.lv>
2014-04-26 02:32:27 +0200

    add Apply Clips.jsx

---
+++ Illustrator/Apply Clips.jsx
@@ -0,0 +1,21 @@
+﻿#target illustrator
+#targetengine main
+#include "./lib/l0utils.jsxinc"
+
+var doc = app.activeDocument
+var clipGroups = _.where(doc.groupItems, {'clipped' : true})
+
+_(clipGroups).forEach(function(clipGroup)
+{
+    var clipPath = _.where(clipGroup.pageItems, {'clipping' : true})[0]
+    
+    _.chain(l0.getAllPaths(clipGroup)).reject({'clipping' : true}).forEach(function(path)
+    {
+        tmpGroup = clipGroup.parent.groupItems.add()
+        tmpClipPath = clipPath.duplicate(tmpGroup, ElementPlacement.PLACEATEND)
+        path.move(tmpGroup, ElementPlacement.PLACEATEND)
+        doc.selection = [tmpGroup]        
+        app.executeMenuCommand("Live Pathfinder Crop")
+        app.executeMenuCommand("expandStyle")
+    })
+})

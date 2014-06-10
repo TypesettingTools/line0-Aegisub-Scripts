77faebe3e2a236b0d25c9322f7e320c8e1ce044f
line0 <line0@mail.lv>
2014-06-10 14:30:46 +0200

    add Sort Active Layer By Position.jsx

---
+++ Illustrator/Sort Active Layer By Position.jsx
@@ -0,0 +1,20 @@
+﻿#target illustrator
+#targetengine main
+#include "./lib/l0utils.jsxinc"
+
+l0_sort_pos()
+
+function l0_sort_pos() {
+    var doc = app.activeDocument
+    l0.genGUIDs(doc.selection[0].layer)
+    var pIs = doc.selection[0].layer.pageItems
+    var pIsSort = []
+    
+    for(var i=0; i<pIs.length; i++) {
+        pIsSort.push({'guid' : pIs[i].guid, 'x' : pIs[i].geometricBounds[0], 'y' : -pIs[i].geometricBounds[1], 'idx' : i})
+    }
+    _.chain(pIsSort).sortBy(['y','x']).forEach(function(pISort,i) {
+        match = _.find(pIs, {'guid': pISort.guid})
+        l0.reorderPageItem(match, i-pISort.idx)
+    })
+}

387fac5368ee1e245e1988e8e5168a79fd6061eb
line0 <line0@mail.lv>
2014-07-30 23:13:32 +0200

    Apply Clips: handle compound clipping paths properly; remove temporary  groups; add progress bar

--- Illustrator/Apply Clips.jsx
+++ Illustrator/Apply Clips.jsx
@@ -2,24 +2,37 @@
 #targetengine main
 #include "./lib/l0utils.jsxinc"
 
-var doc = app.activeDocument
-var clipGroups = _.where(doc.groupItems, {'clipped' : true})
+l0_applyClips()
 
-_(clipGroups).forEach(function(clipGroup)
-{
-    
-    var clipPath = _.filter(clipGroup.pageItems, function(pI){
-        // return pI.clipping || pI.typename=="CompoundPathItem" && pI.pathItems[0].clipping})[0]
-        // apparently compound clipping paths always return 0 pathItems which leaves me with no way to determine wether a compound path is a clipping path. bug?
-        return pI.clipping || pI.typename=="CompoundPathItem"})[0]
-    
-    _.chain(l0.getAllPaths(clipGroup)).reject({'clipping' : true}).forEach(function(path)
+function l0_applyClips() {
+    var prog = l0.progressBar("Applying clips...", applyClips_process)
+    prog.start()
+
+    function applyClips_process()
     {
-        tmpGroup = clipGroup.parent.groupItems.add()
-        tmpClipPath = clipPath.duplicate(tmpGroup, ElementPlacement.PLACEATEND)
-        path.move(tmpGroup, ElementPlacement.PLACEATEND)
-        doc.selection = [tmpGroup]        
-        app.executeMenuCommand("Live Pathfinder Crop")
-        app.executeMenuCommand("expandStyle")
-    })
-})
+        var clipGroups = _.where(doc.groupItems, {'clipped' : true})
+        l0.setCompoundPathClipping(doc)
+        _(clipGroups).forEach(function(clipGroup,j)
+        {
+            var paths = l0.getPaths(clipGroup, ["PathItem", "CompoundPathItem"])
+            var clipPath = _.where(paths, {'clipping' : true})[0]
+
+            _.chain(paths).reject({'clipping' : true}).forEach(function(pI,i,pIs)
+            {
+                prog.setStatus("Group: " + (j+1) + "  Path: " + i)
+                tmpGroup = clipGroup.parent.groupItems.add()
+                tmpClipPath = clipPath.duplicate(tmpGroup, ElementPlacement.PLACEATEND)
+                pI.move(tmpGroup, ElementPlacement.PLACEATEND)
+                doc.selection = [tmpGroup]        
+                app.executeMenuCommand("Live Pathfinder Crop")
+                app.executeMenuCommand("expandStyle")
+                while(doc.selection[0].pageItems.length>0) {
+                    doc.selection[0].pageItems[0].move(doc.selection[0].parent, ElementPlacement.PLACEATEND)
+                }
+                prog.setProgress((j+1)*(i+1)/(clipGroups.length*pIs.length))
+            })
+            clipPath.remove()
+        })
+        prog.close()
+    }
+}

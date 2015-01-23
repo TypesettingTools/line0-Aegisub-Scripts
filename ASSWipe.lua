972a2d61a90eb521e4164819c0892226ad2fbf6d
line0 <line0@mail.lv>
2015-01-23 03:29:05 +0100

    ASSWipe: combine non-animated lines that render to an identical result if their times are consecutive (no gaps or overlap)

--- ASSWipe.lua
+++ ASSWipe.lua
@@ -13,6 +13,7 @@ local reportMsg = [[
 Done. Processed %d lines in %d seconds.
 — Cleaned %d lines (%d%%)
 — Removed %d invisible lines (%d%%)
+— Combined %d consecutive identical lines (%d%%)
 — Filtered %d clips and %d occurences of junk data
 — Total space saved: %.2f KB
 ]]
@@ -30,6 +31,20 @@ local defaultSortOrder = [[
 clip_vect, iclip_vect, \q, \p, \k, \kf, \K, \ko, junk, unknown
 ]]
 
+local function mergeLines(lines, start, cmbCnt, bytes)
+    if lines[start].merged then
+        return lines[start], cmbCnt+1, bytes + #lines[start].raw + 1
+    end
+    local merged = lines[start]
+    for i=start+1,lines.n do
+        if not lines[i].merged and lines[i].start_time==merged.end_time then
+            lines[i].merged = true
+            merged.end_time = lines[i].end_time
+        end
+    end
+    return nil, cmbCnt, bytes
+end
+
 function showDialog(sub, sel, res)
     local dlg = {
         main = {
@@ -45,10 +60,12 @@ function showDialog(sub, sel, res)
                                   hint="Removes clips that don't affect the rendered output." },
             removeInvisible =   { class="checkbox", x=0, y=2, width=2, height=1, value=true, config=true, label="Remove invisible lines",
                                   hint="Deletes lines that don't generate any visible output." },
-            removeJunk =        { class="checkbox", x=0, y=3, width=2, height=1, value=true, config=true, label="Remove junk from tag sections",
+            combineLines =      { class="checkbox", x=0, y=3, width=2, height=1, value=true, config=true, label="Combine consecutive identical lines",
+                                  hint="Merges non-animated lines that render to an identical result and have consecutive times (without overlaps or gaps)." },
+            tagSortOrderLabel = { class="label",    x=4, y=3, width=1, height=1, label="Tag sort order: "},
+            removeJunk =        { class="checkbox", x=0, y=4, width=2, height=1, value=true, config=true, label="Remove junk from tag sections",
                                   hint="Removes any 'in-line comments' and things not starting with a \\ from tag sections." },
-            tagSortOrderLabel = { class="label",   x=4, y=3, width=1, height=1, label="Tag sort order: "},
-            tagSortOrder =      { class="textbox", x=4, y=4, width=10, height=3, value=defaultSortOrder, config=true,
+            tagSortOrder =      { class="textbox",  x=4, y=4, width=10, height=3, value=defaultSortOrder, config=true,
                                   hint="Determines the order cleaned tags will be ordered inside a tag section. Resets always go first, transforms last."}
         }
     }
@@ -64,7 +81,7 @@ function showDialog(sub, sel, res)
 end
 
 function process(sub, sel, res)
-    local lines, linesToDelete, delCnt = LineCollection(sub,sel), {}, 0
+    local lines, linesToDelete, delCnt, linesToCombine, cmbCnt = LineCollection(sub,sel), {}, 0, {}, 0
     local debugError, lineCnt = false, #lines.lines
     local tagNames = table.insert(res.filterClips and ASS.tagNames.clips or {},
                                   res.removeJunk and "junk")
@@ -88,9 +105,19 @@ function process(sub, sel, res)
 
         if res.removeInvisible and oldBounds.w == 0 then
             -- remove invisible lines
-            linesToDelete[delCnt+1], delCnt = line.number, delCnt + 1
+            linesToDelete[delCnt+1], delCnt = line, delCnt + 1
             stats.bytes = stats.bytes + #line.raw + 1
         else
+            if res.combineLines and not oldBounds.animated then
+                local hash = oldBounds.firstHash
+                if not linesToCombine[hash] then
+                    linesToCombine[hash] = {line, n=1}
+                else
+                    linesToCombine[hash][linesToCombine[hash].n+1] = line
+                    linesToCombine[hash].n = linesToCombine[hash].n+1
+                end
+            end
+
             -- clean tags
             data:cleanTags(res.cleanLevel, true, res.tagsToKeep, res.tagSortOrder)
             local newBounds = data:getLineBounds()
@@ -127,12 +154,22 @@ function process(sub, sel, res)
             end
         end
     end, true)
+
+    for hash, lines in pairs(linesToCombine) do
+        if lines.n > 1 then
+            table.sort(lines, function(a,b) return a.start_time < b.start_time end)
+            for j=1,lines.n do
+                linesToDelete[delCnt+cmbCnt+1], cmbCnt, stats.bytes = mergeLines(lines, j, cmbCnt, stats.bytes)
+            end
+        end
+    end
+
     lines:replaceLines()
     sub.delete(linesToDelete)
 
     if debugError then Log.dump{"Styles:", lines.styles, "Configuration:", res} end
     Log.warn(reportMsg, lineCnt, os.time()-stats.start, stats.cleaned, 100*stats.cleaned/lineCnt,
-             delCnt, 100*delCnt/lineCnt, stats.clips, stats.junk, stats.bytes/1000)
+             delCnt, 100*delCnt/lineCnt, cmbCnt, 100*cmbCnt/lineCnt, stats.clips, stats.junk, stats.bytes/1000)
 
     if debugError then
         Log.warn([[However, ASSWipe possibly encountered bugs while cleaning.

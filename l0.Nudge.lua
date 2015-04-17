ca1cf16c123c1a16777da4e3ca2596b9ae4c986d
line0 <line0@mail.lv>
2015-04-17 18:20:40 +0200

    Nudge: bump version; update feeds

--- l0.Nudge.lua
+++ l0.Nudge.lua
@@ -1,20 +1,20 @@
 script_name="Nudge"
 script_description="Provides configurable and hotkeyable tag/line modification macros."
-script_version="0.3.3"
+script_version="0.3.4"
 script_author="line0"
 script_namespace="l0.Nudge"
 
 local DependencyControl = require "l0.DependencyControl"
 local version = DependencyControl{
-    feed = "https://raw.githubusercontent.com/TypesettingCartel/line0-Aegisub-Scripts/master/DependencyControl.json",
+    feed = "https://raw.githubusercontent.com/TypesettingTools/line0-Aegisub-Scripts/master/DependencyControl.json",
     {
         "aegisub.util", "aegisub.clipboard", "json",
         {"a-mo.LineCollection", version="1.0.1", url="https://github.com/torque/Aegisub-Motion"},
         {"a-mo.Log", url="https://github.com/torque/Aegisub-Motion"},
-        {"l0.ASSFoundation", version="0.1.1", url="https://github.com/TypesettingCartel/ASSFoundation",
-         feed = "https://raw.githubusercontent.com/TypesettingCartel/ASSFoundation/master/DependencyControl.json"},
-        {"l0.ASSFoundation.Common", version="0.1.1", url="https://github.com/TypesettingCartel/ASSFoundation",
-         feed = "https://raw.githubusercontent.com/TypesettingCartel/ASSFoundation/master/DependencyControl.json"}
+        {"l0.ASSFoundation", version="0.2.5", url="https://github.com/TypesettingTools/ASSFoundation",
+         feed = "https://raw.githubusercontent.com/TypesettingTools/ASSFoundation/master/DependencyControl.json"},
+        {"l0.ASSFoundation.Common", version="0.1.1", url="https://github.com/TypesettingTools/ASSFoundation",
+         feed = "https://raw.githubusercontent.com/TypesettingTools/ASSFoundation/master/DependencyControl.json"}
     }
 }
 
